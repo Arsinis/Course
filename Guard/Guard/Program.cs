6d9477be6d6884f11b8f33c8c9f2c62e44231fe7
Andrey <andrey.bobrov.95@mail.ru>
2016-05-17 20:25:55 +0300

    Beta release

--- Guard/Guard/Program.cs
+++ Guard/Guard/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
 using Microsoft.Win32;
 
 namespace Guard
@@ -12,10 +13,36 @@ namespace Guard
     {
         public static List<String> data, tokens;
         public static String error = "Oops, something go wrong";
+        public static string attack;
+        public static void Manager()
+        {
+            StreamReader sr = new StreamReader("e:\\1.txt");
+            attack = sr.ReadLine();
+            sr.Close();
+            Console.WriteLine("How to proceed?");
+            Console.WriteLine("1.Quick scan");
+            Console.WriteLine("2.Deep scan");
+            int nc = Int32.Parse(Console.ReadLine());
+            switch (nc)
+            {
+                case 1:
+                    {
+                        Analyzers sc = new Analyzers();
+                        sc.QuickScan(data, attack);
+                    }
+                    break;
+                case 2:
+                    {
+                        Analyzers sc = new Analyzers();
+                        sc.DeepScan(data, attack);
+                    }
+                    break;
+            }
+        }
         public static void Main(string[] args)
         {
-            RegistryKey hklm = Registry.LocalMachine;
-            String path = "e:\\base.txt", attack;
+            
+            String path = "e:\\base.txt";
             FIleWorker fw = new FIleWorker();
             data = fw.Read(path);
             Console.WriteLine("Type of scan");
@@ -27,33 +54,14 @@ namespace Guard
                 case 1:
                     {
                         Scanners sc = new Scanners();
-                        sc.Scan(hklm);
+                        sc.Scan();
+                        sc.GenerateAttack();
+                        Program.Manager();
                     }
                     break;
                 case 2:
                 {
-                    StreamReader sr = new StreamReader("e:\\1.txt");
-                    attack = sr.ReadLine();
-                    sr.Close();
-                    Console.WriteLine("How to proceed?");
-                    Console.WriteLine("1.Quick scan");
-                    Console.WriteLine("2.Deep scan");
-                    int nc = Int32.Parse(Console.ReadLine());
-                    switch (nc)
-                    {
-                        case 1:
-                        {
-                            Analyzers sc = new Analyzers();
-                            sc.QuickScan(data, attack);
-                        }
-                        break;
-                        case 2:
-                        {
-                            Analyzers sc = new Analyzers();
-                            sc.DeepScan(data, attack);
-                        }
-                        break;
-                    }
+                    Program.Manager();
                 }
                 break;
             }
--- Guard/Guard/Scanners.cs
+++ Guard/Guard/Scanners.cs
@@ -1,27 +1,171 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Win32;
 using System.IO;
-using 
+using System.Threading;
+using PacketDotNet;
+using SharpPcap;
+using System.Timers;
+using System.Security.Permissions;
 
 namespace Guard
 {
     public class Scanners
     {
-        public void Scan(RegistryKey hklm)
+        public SortedSet<string> ports = new SortedSet<string>();
+
+        public SortedSet<string> ips = new SortedSet<string>();
+
+        RegistryKey hklm = Registry.LocalMachine;
+
+        public long Count = 0;
+
+        public long RegChan = 0;
+
+        public System.Timers.Timer myTimer = new System.Timers.Timer();
+
+        public CaptureDeviceList deviceList;
+
+        public ICaptureDevice captureDevice;
+
+        public FileSystemWatcher watcher;
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            RegChan++;
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            RegChan++;
+        }
+
+        private void Program_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            Count++;
+            // парсинг всего пакета
+            Packet packet = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+            
+            // получение только TCP пакета из всего фрейма
+            var tcpPacket = TcpPacket.GetEncapsulated(packet);
+            // получение только IP пакета из всего фрейма
+            var ipPacket = IpPacket.GetEncapsulated(packet);
+            if (tcpPacket != null && ipPacket != null)
+            {
+                DateTime time = e.Packet.Timeval.Date;
+                int len = e.Packet.Data.Length;
+
+                // IP адрес отправителя
+                var srcIp = ipPacket.SourceAddress.ToString();
+                ips.Add(srcIp);
+                // IP адрес получателя
+                //var dstIp = ipPacket.DestinationAddress.ToString();
+
+                // порт отправителя
+                //var srcPort = tcpPacket.SourcePort.ToString();
+                // порт получателя
+                var dstPort = tcpPacket.DestinationPort.ToString();
+                ports.Add(dstPort);
+                // данные пакета
+                //var data = tcpPacket.PayloadPacket;
+            }
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            captureDevice.OnPacketArrival -=Program_OnPacketArrival;
+            watcher.Changed -= OnChanged;
+            watcher.Created -= OnChanged;
+            watcher.Deleted -= OnChanged;
+            watcher.Renamed -= OnRenamed;
+            captureDevice.StopCapture();
+        }
+
+        public void Scan()
+        {
+            myTimer = new System.Timers.Timer(4100);
+            myTimer.Elapsed += Timer_Elapsed;
+            myTimer.AutoReset = false;
+            myTimer.Start();
+
+            watcher = new FileSystemWatcher();
+            watcher.Path = "C:/Windows";
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            watcher.Changed += OnChanged;
+            watcher.Created += OnChanged;
+            watcher.Deleted += OnChanged;
+            watcher.Renamed += OnRenamed;
+
+            watcher.EnableRaisingEvents = true;
+
+            deviceList = CaptureDeviceList.Instance;
+            captureDevice = deviceList[1];
+            captureDevice.OnPacketArrival += Program_OnPacketArrival;
+            captureDevice.Open(DeviceMode.Promiscuous, 1000);
+            captureDevice.StartCapture();
+
+        }
+
+        public void GenerateAttack()
         {
             string attack = "";
+            #region traffic
+            if (Count == 0)
+                attack += "traffic=none and ";
+            else if (Count > 50)
+                attack += "traffic=small and ";
+            else if (Count > 100)
+                attack += "traffic=medium and ";
+            else if (Count > 500)
+                attack += "traffic=large and ";
+            #endregion
+
+            #region portimpact
+            if (ports.Count() == Count)
+                attack += "portimpact=little and";
+            else if (ports.Count() < Count / 10)
+                attack += "portimpact=medium and";
+            else if (ports.Count() < Count / 100)
+                attack += "portimpact=big and";
+            #endregion
+
+            #region workwithregistry
             RegistryKey hklmnow = Registry.LocalMachine;
             if (hklmnow == hklm)
-                attack += "workwithregistry=no and "; 
+                attack += "workwithregistry=no and ";
             else
                 attack += "workwithregistry=yes and ";
+            #endregion
 
+            #region elevation
 
-            StreamWriter sw = new StreamWriter(@"e:\\1.txt");
+            if (RegChan == 0)
+                attack += "elevation=no and ";
+            else
+                attack += "elevation=yes and";
+            #endregion
+
+            #region CanalsOpen
+            if (ips.Count() == Count)
+                attack += "CanalsOpen=little and";
+            else if (ips.Count() < Count / 10)
+                attack += "CanalsOpen=average and";
+            else if (ips.Count() < Count / 100)
+                attack += "CanalsOpen=much and";
+            #endregion
+
+            #region identificate
+            if (ips.Contains("192.168.100.1"))
+                attack += "identificate=no";
+            else
+                attack += "identificate=yes";
+            #endregion
+            StreamWriter sw = new StreamWriter("e:\\1.txt");
             sw.Write(attack);
             sw.Close();
         }
